fc48c5816c5d57e3783910a55a5cc1cecd6e2925
phamthienckc <phamthienckc@gmail.com>
2020-08-01 22:50:41 +0700

    Update khóa học của tôi
    Chỉnh sửa khóa học của tôi
    Chỉnh sửa khóa học

--- web-education/resources/views/KhoaHoc/khoa-hoc-cua-toi.blade.php
+++ web-education/resources/views/KhoaHoc/khoa-hoc-cua-toi.blade.php
@@ -20,7 +20,7 @@
     </div>
     <!-- End Breadcrumb -->
 
-    <!-- Start Blog
+    {{--  <!-- Start Blog
     ============================================= -->
     <div class="blog-area single full-blog left-sidebar full-blog default-padding">
         <div class="container">
@@ -50,7 +50,7 @@
 
                                                     <tr>
                                                         <td>
-                                                            {{--  {{$dsKhoaHoc->khoaHoc->ten_khoa_hoc}}  --}}
+
                                                             <img src="{{ asset('assets/images/'.$dsKhoaHoc->khoaHoc->hinh_anh) }}" alt="Thumb" width="100px" height="100px">
                                                         </td>
                                                         <td>{{$dsKhoaHoc->khoaHoc->ten_khoa_hoc}}</td>
@@ -81,5 +81,83 @@
             </div>
         </div>
     </div>
-    <!-- End Blog -->
+    <!-- End Blog -->  --}}
+
+
+    <!-- Start Popular Courses
+        ============================================= -->
+        <div class="popular-courses-area weekly-top-items default-padding bottom-less">
+            <div class="container">
+                <div class="row">
+                    <div class="top-course-items">
+                        <!-- Single Item -->
+                        @foreach($hocVien->hoaDon as $dsHocVien)
+                            @foreach($dsHocVien->ctHoaDon as $dsKhoaHoc)
+                        <div class="col-md-4 col-sm-6 equal-height">
+                            <div class="item">
+                                <div class="thumb">
+                                    <img class="img-khoa-hoc" src="{{ asset('assets/images/'.$dsKhoaHoc->khoaHoc->hinh_anh) }}" alt="Thumb">
+                                    <div class="overlay">
+                                        <a href="{{ action('KhoaHocController@chiTietGiangVien' , $dsKhoaHoc->khoaHoc->giangVien->id) }}">
+                                            <img src="{{ asset('assets/images/'.$dsKhoaHoc->khoaHoc->giangVien->anh_dai_dien) }}" alt="Thumb">
+                                        </a>
+                                        <ul>
+                                            <li><i class="fas fa-clock"></i> 04:15:38</li>
+                                            <li><i class="fas fa-list-ul"></i> {{$dsKhoaHoc->khoaHoc->dsChuongBai->count()}}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="info">
+                                    <div class="meta">
+                                        <ul>
+                                            <li>
+                                                <a class="lam-gon-ten" style="display: -webkit-box;" href="{{ action('KhoaHocController@chiTietGiangVien' , $dsKhoaHoc->khoaHoc->giangVien->id) }}">@if($dsKhoaHoc->khoaHoc->nguoiDung->toChuc->count() > 0) {{ $dsKhoaHoc->khoaHoc->nguoiDung->toChuc[0]->ten_to_chuc }} @else {{$dsKhoaHoc->khoaHoc->giangVien->ho_ten }} @endif</a>
+
+                                            </li>
+                                            <li>
+                                                @if($dsKhoaHoc->khoaHoc->danhGiaKH->count() != 0)
+                                                    {{ round( ($dsKhoaHoc->khoaHoc->danhGiaKH->sum('so_sao') / $dsKhoaHoc->khoaHoc->danhGiaKH->count()), 1, PHP_ROUND_HALF_EVEN)}}
+                                                @else
+                                                    0
+                                                @endif
+                                                <i class="fas fa-star"></i>
+                                            </li>
+
+                                        </ul>
+                                        <ul>
+                                            <li>
+                                                <a href="/linh-vuc/{{$dsKhoaHoc->khoaHoc->LinhVuc->id}}"> {{ $dsKhoaHoc->khoaHoc->LinhVuc->ten_linh_vuc }}</a>
+                                            </li>
+                                            <li>
+                                                <span>({{$dsKhoaHoc->khoaHoc->danhGiaKH->count()}} đánh giá)</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <h4>
+                                        <a href="{{ action('KhoaHocController@hienThiChiTietKhoaHoc' , $dsKhoaHoc->khoaHoc->id) }}" class="lam-gon-van-ban-mo-ta"> {{$dsKhoaHoc->khoaHoc->ten_khoa_hoc}}</a>
+                                    </h4>
+                                    <p class="lam-gon-van-ban-mo-ta">
+                                        {{$dsKhoaHoc->khoaHoc->mo_ta_ngan }}
+                                    </p>
+                                    <div class="footer-meta">
+                                        <h5 style="height: 20px">Tiến độ</h5>
+                                        <div class="do-rong-tien-do">
+                                            <div class="progress">
+                                                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                                                50% hoàn thành
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            @endforeach
+                        @endforeach
+                        <!-- Single Item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Popular Courses -->
 @endsection
